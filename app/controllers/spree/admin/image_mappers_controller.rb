bf944a023af6365017768151e545edb40d9aafba
hemanthmraju <kumarhemanth.cs@gmail.com>
2014-05-23 11:06:34 +0530

    pin tool added

--- app/controllers/spree/admin/image_mappers_controller.rb
+++ app/controllers/spree/admin/image_mappers_controller.rb
@@ -9,4 +9,29 @@ class Spree::Admin::ImageMappersController < ApplicationController
             format.js
      end
     end
+
+    def edit
+          @image_mapper=Spree::ImageMapper.find(params[:id])   
+         respond_to do |format|
+            format.js
+     end
+    end
+
+    def update
+        @image_mapper=Spree::ImageMapper.find(params[:id])
+        if @image_mapper.update_attributes(x1: params[:x1],y1: params[:y1],x2: params[:x2],y2: params[:y2],link: params[:link],alt: params[:alt],shape: params[:shape])   
+            respond_to do |format|
+            format.js
+            end
+        end
+    end
+
+    def destroy
+        @image_mapper=Spree::ImageMapper.find(params[:id])
+        if @image_mapper.destroy
+            respond_to do |format|
+            format.js
+            end
+        end    
+    end    
 end
